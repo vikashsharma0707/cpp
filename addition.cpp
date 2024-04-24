0836da4be2c15a427a8dc8218416ae86ab518e6c
vikash sharma <vikashtiwari9893@gmail.com>
2024-04-24 23:55:58 +0530

    sixth commit

---
+++ addition.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+	int a,b;
+	int sum=0;
+	a=4;
+	b=5;
+	sum=a+b;
+	cout<<sum;
+}
---
+++ evennumber.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int a,b;
+	cout<<"enter a number";
+	cin>>a;
+	cout<<"enter b number";
+	cin>>b;
+	
+	if(a%2==0){
+		cout<<"a is even number";
+	}
+	else{
+		cout<<"b is odd number";
+	}
+}
